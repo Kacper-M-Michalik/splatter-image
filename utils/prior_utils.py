4e9faf42f939c7f7a2c3dca2286244eb404378c5
RadhikaI <Radhika.iyer265@gmail.com>
2025-12-02 13:24:01 +0000

    Add manual LoRA integration code, to be refactored

--- utils/prior_utils.py
+++ utils/prior_utils.py
@@ -26,8 +26,11 @@ def graft_weights_with_channel_expansion(old_state_dict, new_model, old_cfg, new
     # Iterate over all layers
     for name, new_param in new_state_dict.items():
         if name not in old_state_dict:
+            # New LoRA parameters not in base model checkpoint. Skip to avoid KeyError.
+            if "lora_" in name:
+                continue
             print("Failed to find layer {} in HuggingFace model state_dict".format(name))
-            raise "Mismatched source model for graft"
+            raise Exception("Mismatched source model for graft")
 
         old_param = old_state_dict[name]
 
@@ -38,17 +41,21 @@ def graft_weights_with_channel_expansion(old_state_dict, new_model, old_cfg, new
 
         # In theory we should only reach here for Conv2D layers, as such only need to handle weights, and these should only have extra channels in shape[1]
         if ('weight' in name):
-            assert new_param.shape[0] == old_param.shape[0], "Grafting only supported for adding channels, not changing resolution"
-            assert new_param.shape[1] > old_param.shape[1], "Cannot truncate channels during graft, can only add channels"
+            # Dimension check for Conv2D weights
+            if new_param.dim() == 4 and old_param.dim() == 4:
+                assert new_param.shape[0] == old_param.shape[0], "Grafting only supported for adding channels, not changing resolution"
+                assert new_param.shape[1] > old_param.shape[1], "Cannot truncate channels during graft, can only add channels"
 
-            new_weights = new_param.clone()
-            new_weights[:, :old_param.shape[1], :, :] = old_param
-            new_weights[:, old_param.shape[1]:, :, :] = 0.0
+                new_weights = new_param.clone()
+                new_weights[:, :old_param.shape[1], :, :] = old_param
+                new_weights[:, old_param.shape[1]:, :, :] = 0.0
 
-            new_state_dict[name] = new_weights
+                new_state_dict[name] = new_weights
+            else:
+                 print(f"Warning: dimension mismatch (New: {new_param.shape}, Old: {old_param.shape})")
         else:
             print("Failed to graft layer: {}, Old: {}, New: {}".format(name, old_param.shape, new_param.shape))
-            raise "Failed layer graft"
+            raise Exception("Failed layer graft")
             
     new_model.load_state_dict(new_state_dict)
     return new_model
